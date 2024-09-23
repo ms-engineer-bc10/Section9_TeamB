1d26b86fa1d17c05e557dd30a9a1e6c891d2f5e9
ayumihisatsune <imuyaenst2024@gmail.com>
2024-09-23 21:57:26 +0900

    add: フロントエンドの決済画面への遷移

--- frontend/src/app/plan/page.tsx
+++ frontend/src/app/plan/page.tsx
@@ -1,6 +1,8 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
-const plan = () => {
+const Plan = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold mb-8">プラン選択</h1>
@@ -9,11 +11,14 @@ const plan = () => {
         <div className="p-4 border border-black rounded-lg w-64 text-center">
           <h2 className="text-xl font-semibold">有料プラン</h2>
           <p>有料プランの詳細説明がここに入ります。</p>
-          <Link href="/message">
-            <p className="mt-4 block bg-blue-500 text-white py-2 px-4 rounded">
+          <form action="/create-checkout-session" method="POST">
+            <button
+              type="submit"
+              className="mt-4 block bg-blue-500 text-white py-2 px-4 rounded"
+            >
               有料プランへ
-            </p>
-          </Link>
+            </button>
+          </form>
         </div>
 
         <div className="p-4 border border-black rounded-lg w-64 text-center">
@@ -30,4 +35,29 @@ const plan = () => {
   );
 };
 
-export default plan;
+const Message = ({ message }: { message: string }) => (
+  <section>
+    <p>{message}</p>
+  </section>
+);
+
+export default function App() {
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get("success")) {
+      setMessage("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+
+  return message ? <Message message={message} /> : <Plan />;
+}
