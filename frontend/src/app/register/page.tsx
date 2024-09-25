5bfff6ecf2c5ccac9d2b9e6d9a056007d96f081a
ayumihisatsune <imuyaenst2024@gmail.com>
2024-09-25 22:57:13 +0900

    add: ログインステータス確認のためHeaderを追加

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -37,7 +37,7 @@ const Register: React.FC = () => {
 
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <div className="min-h-screen flex items-center justify-center">
         <form
           onSubmit={handleSubmit(onSubmit)}
