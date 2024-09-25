c8ca394f0d8eb317c3612b59013a65f25c909419
hayakak <hayakasaito@gmail.com>
2024-09-26 02:08:35 +0900

    コメントアウト戻すbackend\tellings\models.py

--- backend/tellings/models.py
+++ backend/tellings/models.py
@@ -3,17 +3,11 @@ from django.conf import settings
 from accounts.models import Child
 
 class TellingRecord(models.Model):
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='telling_records')
-    # child = models.ForeignKey('accounts.Child', on_delete=models.CASCADE)
-    # book = models.ForeignKey('book_creation.Book', on_delete=models.CASCADE, related_name='telling_records')
     telling_date = models.DateTimeField()
     child_reaction = models.TextField()
     notes = models.TextField()
 
 class TellingReminder(models.Model):
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='telling_reminders')
-    # child = models.ForeignKey('accounts.Child', on_delete=models.CASCADE)
-    # book = models.ForeignKey('book_creation.Book', on_delete=models.CASCADE, related_name='telling_reminders')
     reminder_date = models.DateTimeField()
     is_sent = models.BooleanField(default=False)
 
