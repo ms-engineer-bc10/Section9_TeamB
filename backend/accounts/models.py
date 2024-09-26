05b06544464e80c015b9fa2eb36518d6457dbfbf
m-hiraoka <hiraoka.miyako@gmail.com>
2024-09-27 00:40:57 +0900

    add : CHILDの一部フィールドを選択式に変更

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -51,15 +51,34 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = 'users'
 
 class Child(models.Model):
+    GENDER_CHOICES = [
+        ('boy', '男の子'),
+        ('girl', '女の子'),
+        ('no_answer', '答えたくない'),
+    ]
+    
+    BACKGROUND_CHOICES = [
+        ('special_adoption', '特別養子縁組'),
+        ('foster_regular_adoption', '里子・普通養子縁組'),
+        ('sperm_donation', '精子提供'),
+        ('egg_donation', '卵子提供'),
+        ('step_family', 'ステップファミリー'),
+        ('other', 'その他'),
+    ]
+        
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='children')
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=30)
     birth_date = models.DateField()
     arrival_date = models.DateField(null=True, blank=True)
-    gender = models.CharField(max_length=20)
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES)
     interests = models.TextField(null=True, blank=True)
-    background_type = models.CharField(max_length=20)
+    background_type = models.CharField(max_length=30, choices=BACKGROUND_CHOICES)
+    background_other = models.CharField(max_length=225, null=True, blank=True, help_text="「その他」を選択した場合、詳細を記入してください。")
     origin_background = models.TextField(null=True, blank=True)
     care_background = models.TextField(null=True, blank=True)
     family_structure = models.TextField(null=True, blank=True)
-    father_title = models.CharField(max_length=50, null=True, blank=True)
-    mother_title = models.CharField(max_length=50, null=True, blank=True)
+    father_title = models.CharField(max_length=20, null=True, blank=True)
+    mother_title = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
