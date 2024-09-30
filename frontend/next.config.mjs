a8b56b461d92372b744b43f021238552643ada32
hayakak <hayakasaito@gmail.com>
2024-09-30 21:37:13 +0900

    nextconfig.mjs　画像追加のため加筆

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['i.imgur.com'], // Imgurからの画像を許可
+    },
+  };
+  
+  export default nextConfig;
+  
