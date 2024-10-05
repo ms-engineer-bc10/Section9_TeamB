2a5ade2e5c681c5a2fe60b4f6d8fc621173942ce
ayumihisatsune <imuyaenst2024@gmail.com>
2024-10-05 09:02:27 +0900

    update: api.tsを活用/handleFormSubmitを削除

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,1 +1,37 @@
-//バックエンドAPIとの通信処理
+import { apiUrl } from "@/lib/config";
+
+// 子供情報をPOSTするAPIリクエスト
+export const createChild = async (token: string, postData: any) => {
+  const response = await fetch(`${apiUrl}/api/children/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, // Bearer形式でトークンを送信
+    },
+    body: JSON.stringify(postData),
+  });
+
+  if (!response.ok) {
+    throw new Error("子供情報の送信に失敗しました");
+  }
+
+  return response.json();
+};
+
+// 絵本生成リクエストのAPIリクエスト
+export const createBook = async (token: string, childId: number) => {
+  const response = await fetch(`${apiUrl}/api/books/create_book/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ child_id: childId }),
+  });
+
+  if (!response.ok) {
+    throw new Error("絵本生成リクエストに失敗しました");
+  }
+
+  return response.json();
+};
