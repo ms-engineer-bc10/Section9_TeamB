11d2d383ee008ebd62ff13a9632d24040de4c727
m-hiraoka <hiraoka.miyako@gmail.com>
2024-10-12 20:19:59 +0900

    add：task.pyの定義

--- backend/picturebook/settings.py
+++ backend/picturebook/settings.py
@@ -216,3 +216,13 @@ LOGGING = {
         },
     },
 }
+
+# Celery settings
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+
+INSTALLED_APPS += ['django_celery_results']
