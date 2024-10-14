0a46fe035f5f9f9c65457cfeaa2b0af581012f03
ayumihisatsune <imuyaenst2024@gmail.com>
2024-10-14 18:24:12 +0900

    add: 子ども編集フォームから絵本作成

--- frontend/src/app/children/[id]/page.tsx
+++ frontend/src/app/children/[id]/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { getChildId, updateChild } from "@/lib/api";
+import { getChildId, updateChild, createBook } from "@/lib/api";
 import ChildForm from "@/components/EditChild/ChildForm";
-import Header from "@/components/Header";
+import { auth } from "@/lib/firebase";
 import { useRedirectIfNotAuthenticated } from "@/lib/auth";
 import Loading from "@/components/Loading";
 
@@ -25,6 +25,9 @@ export default function EditChild({ params }: { params: { id: string } }) {
   });
   const [showArrivalDate, setShowArrivalDate] = useState(true);
   const [showBackgroundOther, setShowBackgroundOther] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [message, setMessage] = useState<string | null>(null);
 
   const router = useRouter();
 
@@ -79,13 +82,44 @@ export default function EditChild({ params }: { params: { id: string } }) {
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    // 子ども情報をPUT
-    const response = await updateChild(params.id, formData);
+    setIsLoading(true);
+    setError(null);
+    setMessage(null);
 
-    if (response.ok) {
-      router.push("/home");
-    } else {
-      alert("Error updating child.");
+    try {
+      const currentUser = auth.currentUser;
+
+      if (!currentUser) {
+        console.error("ユーザーがログインしていません");
+        return;
+      }
+
+      const token = await currentUser.getIdToken(true);
+
+      // PUTで子ども情報を更新
+      const response = await updateChild(params.id, formData);
+
+      if (!response.ok) {
+        throw new Error("子ども情報の更新に失敗しました。");
+      }
+
+      // 絵本生成リクエスト
+      await createBook(token, Number(params.id));
+
+      // 絵本生成中のメッセージを表示
+      setMessage("絵本を生成中です。完了しましたらメールでお知らせします。");
+
+      // 5秒後にホームにリダイレクト
+      setTimeout(() => {
+        router.push("/home");
+      }, 5000);
+    } catch (error) {
+      console.error("APIリクエスト中にエラーが発生しました:", error);
+      setError(
+        error instanceof Error ? error.message : "予期せぬエラーが発生しました"
+      );
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -98,6 +132,9 @@ export default function EditChild({ params }: { params: { id: string } }) {
           handleSubmit={handleSubmit}
           showArrivalDate={showArrivalDate}
           showBackgroundOther={showBackgroundOther}
+          isLoading={isLoading}
+          error={error}
+          message={message}
         />
       ) : (
         <Loading />
